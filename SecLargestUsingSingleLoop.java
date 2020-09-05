3716f82497847fba253c0590750d96bc3df9d2ac
plalbesu <plal.besu@gmail.com>
2020-09-05 11:34:30 +0530

    This file has been submitted by Naveen

---
+++ SecLargestUsingSingleLoop.java
@@ -0,0 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SecLargestUsingSingleLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		secLargest();
+
+	}
+	
+	
+	
+	public static void secLargest() {
+		
+		
+		/*
+		 * Scanner sc = new Scanner(System.in);
+		 * System.out.println("Enter the number of Element in Array"); int eleCount =
+		 * sc.nextInt(); if(eleCount<2) {
+		 * System.out.println("\nSorry, Number of Element Should be more than one.");
+		 * System.exit(0); } int[] arr = new int[eleCount];
+		 * System.out.println("Enter Element in Array" ); for(int i=0;i<eleCount;i++) {
+		 * arr[i]=sc.nextInt(); }
+		 */
+			int[] arr = {0,-9,-5,-4,-4,-8,-2,-11};
+			for(int i = 1;i<arr.length;i++) {
+				if(arr[i] > arr[i-1] ) {
+					arr[i]=arr[i]+arr[i-1];
+					arr[i-1]= arr[i]-arr[i-1];
+					arr[i]=arr[i]-arr[i-1];
+					i=0;
+					 
+				}
+				
+			}
+			System.out.println("Second Largest Element: "+arr[1]);								
+	}
+
+}
