7da6f5f33a0a646546c8db378f474df3af3d779d
12345puja <56876517+12345puja@users.noreply.github.com>
2020-09-08 18:30:52 +0530

    Add files via upload

---
+++ deletion.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class deletion
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements of array:");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter position to be deleted:");
+        int pos = sc.nextInt();
+        int[] newarr = new int[n-1];
+        for(int i=0; i<n-1; i++){
+            if(i<pos)
+               newarr[i] = arr[i];
+            else if(i>=pos)
+               newarr[i] = arr[i+1];
+        }
+        for(int i=0; i<n-1; i++){
+            System.out.print(newarr[i]+" ");
+        }
+    }
+}
